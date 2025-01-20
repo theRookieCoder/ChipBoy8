226bb4c2e012880654e127e28f3e85b5dbe6e681
Ilesh Thiada <ileshkt@gmail.com>
2025-01-20 13:27:40 +0530

    Initial commit

---
+++ ChipBoy8.ino
@@ -0,0 +1,13 @@
+extern "C" {
+#include "core.h"
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  MachineState* state = core_init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ core.h
@@ -0,0 +1,1 @@
+CChip8/src/core.h
